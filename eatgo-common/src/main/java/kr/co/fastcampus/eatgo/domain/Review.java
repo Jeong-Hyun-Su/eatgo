e49e580bd9cf83b66cfb06361c6dfb284ea15dba
Jeong-Hyun-Su <mses1572@naver.com>
2020-08-04 18:07:18 +0900

    maria db init

--- eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
+++ eatgo-common/src/main/java/kr/co/fastcampus/eatgo/domain/Review.java
@@ -8,6 +8,7 @@ import javax.persistence.Id;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Builder
@@ -20,6 +21,7 @@ public class Review {
     @GeneratedValue
     private Integer id;
 
+    @NotNull
     @Setter
     private Integer restaurantId;
 
